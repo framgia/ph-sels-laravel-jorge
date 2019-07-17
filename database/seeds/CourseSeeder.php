895374bc98dca7c8ac0b4effc4ebb6c3145c106c
Jorge <jorgelim40@gmail.com>
2019-07-17 11:34:00 +0800

    Implement Show Course List

---
+++ database/seeds/CourseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CourseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('courses')->insert([
+            'title' => Str::random(30),
+            'description' => Str::random(300)
+        ]);
+    }
+}
